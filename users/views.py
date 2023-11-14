a3b4dbe9a7cf55d0e142446987d31ebbb839b0c5
Natalia Melnik <nataliamelnik138@gmail.com>
2023-11-14 19:27:06 +0600

    Описаны контроллеры для регистрации пользователя, просмотра и редактирования профиля пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from users.models import User
+from users.permissions import IsActivUser
+from users.serliazers import UserCreateSerializer, UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsActivUser]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsActivUser]
