bf3383d9702865c0457bd357b23ec62ea84d9a69
Natalia Melnik <nataliamelnik138@gmail.com>
2023-11-14 19:24:34 +0600

    Модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    avatar = models.ImageField(
+        upload_to='users/',
+        blank=True,
+        null=True,
+        verbose_name='Аватар'
+    )
+    telegram_chat_id = models.CharField(max_length=35, verbose_name='ID чата Telegram', blank=True, null=True,)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
