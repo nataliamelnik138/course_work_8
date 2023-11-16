b5cf84af14371628862a57ca42b6667e604564d1
Natalia Melnik <nataliamelnik138@gmail.com>
2023-11-16 19:21:44 +0600

    Добавлены docstrings

--- habits/views.py
+++ habits/views.py
@@ -8,17 +8,18 @@ from habits.serliazers import HabitSerializer, HabitPublicSerializer
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
+    """Эндпоинт создания привычки"""
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         new_habit = serializer.save()
         new_habit.user = self.request.user
-
         new_habit.save()
 
 
 class HabitListAPIView(generics.ListAPIView):
+    """Эндпоинт просмотра списка привычек"""
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated, IsOwner]
     pagination_class = HabitPaginator
@@ -29,6 +30,7 @@ class HabitListAPIView(generics.ListAPIView):
 
 
 class HabitPublicListAPIView(generics.ListAPIView):
+    """Эндпоинт просмотра списка публичных привычек"""
     serializer_class = HabitPublicSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = HabitPaginator
@@ -36,17 +38,20 @@ class HabitPublicListAPIView(generics.ListAPIView):
 
 
 class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    """Эндпоинт просмотра привычки"""
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
     permission_classes = [IsAuthenticated, IsOwner]
 
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
+    """Эндпоинт редактирования привычки"""
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
     permission_classes = [IsAuthenticated, IsOwner]
 
 
 class HabitDestroyAPIView(generics.DestroyAPIView):
+    """Эндпоинт удаления привычки"""
     queryset = Habit.objects.all()
     permission_classes = [IsAuthenticated, IsOwner]
